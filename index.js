94ad1ea13588b3f9e7c1855fe569bcf227468806
Nikhil Rawat <rawatnikita934@gmail.com>
2025-02-12 22:59:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let get =document.getElementById("get")
+async function myFun(){
+    let getting=await fetch('https://icanhazdadjoke.com/' ,{
+      headers :{
+        accept:" application/json"
+      }
+
+      
+    })
+
+    let joke= await getting.json();
+        let data=joke.joke;
+        get.innerHTML=data;
+      console.log(getting)
+    
+}
+myFun
+
+
+
